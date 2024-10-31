1a6ecc412505b21268dda02e1f0470634667413f
MilosJovic23 <milos.mj992@gmail.com>
2024-10-31 11:12:49 +0100

    * small changes

---
+++ vezba3.php
@@ -0,0 +1,20 @@
+
+
+<?php
+
+
+
+
+
+    $reci = ["Slovo","Asembley","PHP","Java","C","TypeScript"];
+
+    foreach($reci as $rec){
+
+        $duzinaReci = strlen($rec);
+
+        if ($duzinaReci > 5){
+           echo "Rec $rec ima preko 5 karaktera";
+        }
+    }
+
+?>
